59e21070be7dd183836d8fdf20993fb40886d310
tagazok <leplus.olivier@gmail.com>
2021-10-10 13:06:30 +0200

    feat: support multiple talks in an event Also added roadshow

--- assets/app.js
+++ assets/app.js
@@ -6,76 +6,102 @@ const dom_btn_moreconferences = document.querySelector('#more-conferences');
 const dom_videos = document.querySelector('#videos');
 
 dom_btn_moreconferences.addEventListener('click', event => {
-    console.log("View more conferences");
-    document.querySelector('.conferences__overlay').remove();
-    dom_btn_moreconferences.remove();
-    dom_conferences.classList.remove("conferences--small");
+  console.log("View more conferences");
+  document.querySelector('.conferences__overlay').remove();
+  dom_btn_moreconferences.remove();
+  dom_conferences.classList.remove("conferences--small");
 });
 
 
 class Conference {
 
-    /**
-     * @param {any} conference
-     * @param {boolean} past
-     */
-    constructor(conference, past) {
-        this.conference = conference;
-        this.past = past;
+  /**
+   * @param {any} conference
+   * @param {boolean} past
+   */
+  constructor(conference, past) {
+    this.conference = conference;
+    this.past = past;
+  }
+
+  /**
+   * @param {any} past
+   */
+  isPast(past) {
+    if (!past) {
+      return `<span class='soon'>Soon!</span>`
     }
+    return '';
+  }
 
-    /**
-     * @param {any} past
-     */
-    isPast(past) {
-        if (!past) {
-            return `<span class='soon'>Soon!</span>`
-        }
-        return '';
+  getTalk() {
+    if (!this.conference.talk) return "";
+
+    return `<span> • ${this.conference.talk.title}</span>`;
+  }
+
+  getTalks() {
+    if (!this.conference.talks) return "";
+
+    let tpl = '<div class="talks"><ul>';
+    for (let talk of this.conference.talks) {
+      tpl += `
+        <li class="talk">
+          <div class="flex flex-row">
+            <div> ${talk.title}</div>
+            <div class="flex-1"></div>
+            ${this.getTalkVideo(talk)}
+          </div>
+        </li>`;
     }
+    tpl += '</ul></div>';
+    return tpl;
+  }
 
-    getTalk() {
-        if (!this.conference.talk) return "";
+  getRoles() {
+    if (!this.conference.roles || this.conference.roles.length === 0) return "";
 
-        return `<span> • ${this.conference.talk.title}</span>`;
+    let tpl = '';
+    for (let role of this.conference.roles) {
+      tpl += `<li class="role ${role}">[${role}]</li>`;
     }
+    return tpl;
+  }
 
-    getRoles() {
-        if (!this.conference.roles || this.conference.roles.length === 0) return "";
+  getTalkVideo(talk) {
+    if (!talk.video) return "";
+    return `
+    <a target="_blank" title="See video" href="${talk.video}">
+      <i class="fas fa-video"></i>
+    </a>`;
+  }
 
-        let tpl = '';
-        for (let role of this.conference.roles) {
-            tpl += `<li class="role ${role}">[${role}]</li>`;
-        }
-        return tpl;
+  getVideo() {
+      if (!this.conference.talk || !this.conference.talk.video) return "";
+      return `
+    <a target="_blank" title="See video" href="${this.conference.talk.video}">
+      <i class="fas fa-video"></i>
+    </a>`;
+  }
+
+  roadshowLabel() {
+    debugger;
+    if (this.conference.event === 'roadshow') {
+      return `<div class="label">Roadshow</div>`
     }
-
-    getVideo() {
-        if (!this.conference.talk || !this.conference.talk.video) return "";
-        return `
-      <a target="_blank" title="See video" href="${this.conference.talk.video}">
-        <i class="fas fa-video"></i>
-      </a>`;
-    }
-
-    roadshowLabel() {
-      debugger;
-      if (this.conference.event === 'roadshow') {
-        return `<div class="label">Roadshow</div>`
-      }
-    }
-    toHtml() {
-        const tmpl = `
+  }
+  toHtml() {
+    const tmpl = `
             <li class="conference ${this.conference.event || ''}">
               <div class="conference-container">
                 <div class="flex flex-row">
-                  <div>
+                  <div class="width-100">
                     ${this.isPast(this.past)}
                     <ul class="roles">
                       <li class="role speaker">${this.getRoles()}</li>
                     </ul>
                     <a href="${this.conference.link}" class="host" target="_blank">${this.conference.name}</a>
-                    ${this.getTalk()}
+                    ${this.getTalks()}
                   </div>
                   <div class="flex-1"></div>
                   ${this.getVideo()}
@@ -88,46 +114,46 @@ class Conference {
               </div>
             </li>
           `;
-        const range = document.createRange();
-        const fragment = range.createContextualFragment(tmpl);
+    const range = document.createRange();
+    const fragment = range.createContextualFragment(tmpl);
 
-        return fragment;
-    }
+    return fragment;
+  }
 }
 
 class Conferences {
-    constructor() {
-        /** @type {Conference[]} */
-        this.conferences = [];
+  constructor() {
+    /** @type {Conference[]} */
+    this.conferences = [];
+  }
+
+  async get() {
+    const raw_data = await fetch('assets/data/conferences.json');
+    const data = await raw_data.json();
+
+    if (data) {
+      const currentDate = new Date().getTime();
+
+      for (let conference of data) {
+        const d = new Date(conference.date).getTime();
+        const past = d < currentDate;
+        this.conferences.push(new Conference(conference, past));
+      }
     }
+  }
 
-    async get() {
-        const raw_data = await fetch('assets/data/conferences.json');
-        const data = await raw_data.json();
-
-        if (data) {
-            const currentDate = new Date().getTime();
-
-            for (let conference of data) {
-                const d = new Date(conference.date).getTime();
-                const past = d < currentDate;
-                this.conferences.push(new Conference(conference, past));
-            }
-        }
+  print() {
+    for (let conference of this.conferences) {
+      dom_conferences.insertBefore(conference.toHtml(), dom_conferences.lastChild);
     }
-
-    print() {
-        for (let conference of this.conferences) {
-            dom_conferences.insertBefore(conference.toHtml(), dom_conferences.lastChild);
-        }
-    }
+  }
 }
 
 
 (async function () { // async function expression used as an IIFE
-    const conferences = new Conferences();
-    await conferences.get();
-    conferences.print();
+  const conferences = new Conferences();
+  await conferences.get();
+  conferences.print();
 })();
 
 
