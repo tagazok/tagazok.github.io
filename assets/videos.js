73d9b637e166f836d87c8483465fcccf3daa0605
tagazok <leplus.olivier@gmail.com>
2022-04-29 16:47:13 +0200

    feat(videos): Test videos page

---
+++ assets/videos.js
@@ -0,0 +1,35 @@
+const dom_videos = document.querySelector('#videos');
+
+const videos = [];
+
+function generateVideoTemplate(video) {
+    console.log(video);
+    const tmpl = `
+    <div class="video-container">
+      <a href="https://www.youtube.com/watch?v=${video.youtubeId}" class="video flex flex-column" target="_blank">
+        <img src="https://img.youtube.com/vi/${video.youtubeId}/mqdefault.jpg" alt="${video.title} thumbnail" />
+        <div class="video-title">${video.title}</div>
+        <p class="video-description">${video.duration}<span>•</span>${video.event.name}</p>
+      </a>
+    </div>`;
+
+    const range = document.createRange();
+    const fragment = range.createContextualFragment(tmpl);
+
+    return fragment;
+}
+
+async function getVideos() {
+    const raw_data = await fetch('assets/data/videos.json');
+    const data = await raw_data.json();
+
+    if (data) {
+        for (let video of data) {
+            dom_videos.insertBefore(generateVideoTemplate(video), dom_videos.lastChild);
+        }
+    }
+}
+
+(async function () { // async function expression used as an IIFE
+    getVideos()
+})();
